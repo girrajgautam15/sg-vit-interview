6ffe89f0a6fb0a36a8f80f32b6edc5057688304b
Girraj Gautam <76258250+girrajgautam15@users.noreply.github.com>
2023-08-12 12:26:51 +0530

    Create Girraj.cpp

---
+++ Girraj.cpp
@@ -0,0 +1,27 @@
+//Insertion sort
+//Girraj Gautam
+
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    int n;
+    cin>>n;
+    int arr[n];
+    for(int i=0;i<n;i++){
+        cin>>arr[i];
+    }
+    
+    int j;
+    for(int i=0;i<n;i++){
+        j=i;
+        while(j>0 && arr[j-1]>arr[j]){
+            swap(arr[j-1],arr[j]);
+            j--;
+        }
+    }
+    
+    for(int i=0;i<n;i++){
+        cout<<arr[i]<<" ";
+    }
+    return 0;
+}
